180c713643463fadd2403214c8a993758063b8de
unknown <ram3003200@gmail.com>
2025-05-07 18:14:48 +0530

    adding a register_model proecss

---
+++ scripts/mlflow_test.py
@@ -0,0 +1,17 @@
+import mlflow
+import random
+
+# Set the MLflow tracking URI
+mlflow.set_tracking_uri("http://44.211.177.183:5000/")
+
+# Start an MLflow run
+with mlflow.start_run():
+    # Log some random parameters
+    mlflow.log_param("param1", random.randint(1, 100))
+    mlflow.log_param("param2", random.random())
+
+    # Log some random metrics
+    mlflow.log_metric("metric1", random.random())
+    mlflow.log_metric("metric2", random.uniform(0.5, 1.5))
+
+    print("Logged random parameters and metrics.")
